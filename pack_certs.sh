a98c0580b9e9c7c4346984eb5182353262581df8
Jiaxin Li <greencodes@163.com>
2023-03-24 12:48:55 +0800

    update

--- pack_certs.sh
+++ pack_certs.sh
@@ -1,15 +1,18 @@
 # request letsencrypt certificates
 SIGNER=1F517725
-RECIPIENT=95377775  # ASTER Cert Deploy
-OUT_DIR="/Users/gcodes/Credentials/public/encrypted"
+RECIPIENT=1F517725  #171DCA62
+OUT_DIR="../vault/ca"
 
 if ! [[ $# == 1 ]]; then echo "Usage: $0 path/to/cert/dir"; exit; fi
-OUT_FILE="$OUT_DIR/$(basename $1).tar.gpg"
+OUT_FILE="$OUT_DIR/$(basename $1).tar.bz2.gpg"
 
 echo "Signing with $SIGNER, encrypting to $RECIPIENT."
-echo -e "Will package $1 to:\n > $OUT_FILE"
+echo "Will package $1 to:\n > $OUT_FILE"
 read -p "Continue? [y/N]: " VAR
 if ! [[ $VAR =~ [Yy] ]]; then exit; fi
 
-cd $(dirname $1)
-tar -cL $(basename $1) | gpg -s -u $SIGNER -e -r $RECIPIENT -o $OUT_FILE
+tar -C $(dirname $1) -c $(basename $1) | pbzip2 | gpg -s -u $SIGNER -e -r $RECIPIENT > $OUT_FILE
+
+read -p "Delete original? [y/N]: " VAR
+if ! [[ $VAR =~ [Yy] ]]; then exit; fi
+rm -rf $1
