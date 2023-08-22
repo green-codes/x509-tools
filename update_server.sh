eb4af10113c8a42625abd29ca47bbd8389a71ee3
Jiaxin Li <greencodes@163.com>
2023-08-22 13:34:55 +0800

    update

--- update_server.sh
+++ update_server.sh
@@ -2,11 +2,16 @@
 
 target="root@asv.ink:/var/www/asv.ink/ca"
 
-# update certificate authority
 mkdir ./ca_out
 for d in $(ls "./ca/"); do 
+    echo "> $d"
     cp "./ca/$d/certs/ca-chain.crt" "./ca_out/$d-chain.crt"
     cp "./ca/$d/crl/ca.crl" "./ca_out/$d.crl"
 done
-rsync --stats -h --delete -z -ur ./ca_out/ $target
+
+read -p "Continue? [y/N]: " VAR
+if [[ $VAR =~ [Yy] ]]; then 
+    rsync --stats -h --delete -z -ur ./ca_out/ $target
+fi
+
 rm -rf ./ca_out
