726a8516571e7e85417897ebb597988de85e7cfd
Jiaxin Li <greencodes@163.com>
2023-08-22 13:17:41 +0800

    bugfix

--- update_server.sh
+++ update_server.sh
@@ -8,5 +8,5 @@ for d in $(ls "./ca/"); do
     cp "./ca/$d/certs/ca-chain.crt" "./ca_out/$d-chain.crt"
     cp "./ca/$d/crl/ca.crl" "./ca_out/$d.crl"
 done
-rsync --stats -h --delete -z -ur ./ca_out $target
+rsync --stats -h --delete -z -ur ./ca_out/ $target
 rm -rf ./ca_out
